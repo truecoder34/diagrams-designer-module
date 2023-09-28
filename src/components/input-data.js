532cf4108f192375c4436f643f80312486772b0e
plotnikov.v10 <plotnikov.v10@wb.ru>
2023-09-29 00:56:45 +0300

    nested routes refactoring

--- src/components/input-data.js
+++ src/components/input-data.js
@@ -21,43 +21,42 @@ import ConstructionElements from './input/construction-elements';
 
 
 export default function InputData() {
-    const [value, setValue] = React.useState(3);
+    const [value, setValue] = React.useState('a');
     const handleChange = (event, newValue) => {
-        console.log("New value renfering :: ", newValue)
-        setValue(newValue);
+        console.log("New value refering :: ", newValue.toString())
+        setValue(newValue.toString());
+        //setValue(newValue);
 
-        subPagesArray.map((x, index) => (
-            console.log(x, index)
-        ))
-        subPagesLinksArray.map((x, index) => (
-            console.log(x, index)
-        ))
+        // subPagesArray.map((x, index) => (
+        //     console.log(x, index)
+        // ))
+        // subPagesLinksArray.map((x, index) => (
+        //     console.log(x, index)
+        // ))
 
     };
     const subPagesArray = ['Показатели качества', 'Элементарные функции',
         'Конструктивные элементы', 'Редактирование значений',]
     const subPagesLinksArray = ['qualityMetrics', 'elementaryFunctions',
         'constructionElements', 'valueChange',]
+    const tabValuesArray = ['a', 'b','c', 'd',]
     return (
         <Box sx={{ width: '100%', typography: 'body1' }}>
-            <TabContext value={value}>
-            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                    <TabList onChange={handleChange} aria-label="lab API tabs example">
-                        {subPagesArray.map((_, index) => (
-                            <Tab
-                                key={index}
-                                component={Link}
-                                to={subPagesLinksArray[index]}
-                                label={subPagesArray[index]} value={index} />
-                        ))}
-                    </TabList>
-                </Box>
-            {/* {subPagesArray.map((num, index) => (
-                    <TabPanel key={index} value={index}> </TabPanel>
-                ))} */}
-            </TabContext>
-
-
+            <Tabs
+                value={value}
+                onChange={handleChange}
+                textColor="secondary"
+                indicatorColor="secondary"
+                aria-label="secondary tabs example"
+            >
+                {subPagesArray.map((_, index) => (
+                    <Tab
+                        key={index}
+                        component={Link}
+                        to={subPagesLinksArray[index]}
+                        label={subPagesArray[index]} value={tabValuesArray[index]} />
+                ))}
+            </Tabs>
 
             <Routes>
                 <Route path=':qualityMetrics' element={<QualityMetrics />} />
