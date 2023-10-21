f65cee8fbfcc43d256d2cd7d18b1b08ca0fae8dd
plotnikov.v10 <plotnikov.v10@wb.ru>
2023-10-21 11:35:48 +0300

    refactoring schemas component

--- src/components/File/Schemes.js
+++ src/components/File/Schemes.js
@@ -76,6 +76,36 @@ export default function Schemes() {
                 <h2>Схемы</h2>
             </div>
 
+            <Grid container rowSpacing={1} columnSpacing={2}>
+                <Grid item xs={12}>
+                    <ImageList sx={{
+                        width: "100%",
+                        height: 600,
+                        // Promote the list into its own layer in Chrome. 
+                        // This costs memory, but helps keeping high FPS.
+                        transform: 'translateZ(0)',
+                    }}
+                        rowHeight={200}
+                        gap={10}>
+
+                        {itemData.map((item) => (
+                            <ImageListItem key={item.img} cols={1} rows={itemData.length}>
+                                <img
+                                    srcSet={`${item.img}?w=500&fit=crop&auto=format&dpr=2 2x`}
+                                    src={`${item.img}?w=500&fit=crop&auto=format`}
+                                    alt={item.title}
+                                    loading="lazy"
+                                />
+                                <ImageListItemBar
+                                    title={item.title}
+                                    position="top"
+                                />
+                            </ImageListItem>
+                        ))}
+                    </ImageList>
+                </Grid>
+            </Grid>
+
 
             <Grid sx={{
                 // display: 'flex', 
@@ -97,12 +127,12 @@ export default function Schemes() {
                         </Box>
                         <TabPanel value="1">
                             <h4>
-                                {itemData.title}
+                                {itemDataFPD[0].title}
                             </h4>
                             <img
-                                srcSet={`${itemData[0].img}`}  //?w=250&h=250&fit=crop&auto=format&dpr=2 2x
-                                src={`${itemData[0].img}`}  // ?w=250&h=250&fit=crop&auto=format
-                                alt={itemData.title}
+                                srcSet={`${itemDataFPD[0].img}`}  //?w=250&h=250&fit=crop&auto=format&dpr=2 2x
+                                src={`${itemDataFPD[0].img}`}  // ?w=250&h=250&fit=crop&auto=format
+                                alt={itemDataFPD[0].title}
                                 loading="lazy"
                             />
                         </TabPanel>
@@ -153,64 +183,81 @@ export default function Schemes() {
             </Grid>
 
 
-            <Grid container rowSpacing={1} columnSpacing={2}>
-                <Grid item xs={12}>
-                    <ImageList sx={{
-                        width: "100%",
-                        height: 600,
-                        // Promote the list into its own layer in Chrome. 
-                        // This costs memory, but helps keeping high FPS.
-                        transform: 'translateZ(0)',
-                    }}
-                        rowHeight={200}
-                        gap={10}>
-
-                        {itemData.map((item) => (
-                            <ImageListItem key={item.img} cols={1} rows={itemData.length}>
-                                <img
-                                    srcSet={`${item.img}?w=500&fit=crop&auto=format&dpr=2 2x`}
-                                    src={`${item.img}?w=500&fit=crop&auto=format`}
-                                    alt={item.title}
-                                    loading="lazy"
-                                />
-                                <ImageListItemBar
-                                    title={item.title}
-                                    position="top"
-                                />
-                            </ImageListItem>
-                        ))}
-                    </ImageList>
+            <Grid sx={{
+                // display: 'flex', 
+                // flexWrap: 'wrap', 
+                maxWidth: '250',
+                height: '250',
+                maxHeight: '250',
+            }}
+                container
+                rowSpacing={1}
+                columnSpacing={2}>
+                <Grid item xs={6}>
+                    <TabContext value={value}>
+                        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                            <TabList onChange={handleChange} aria-label="lab API tabs example">
+                                <Tab label="Схема" value="1" />
+                                <Tab label="Описание" value="2" />
+                            </TabList>
+                        </Box>
+                        <TabPanel value="1">
+                            <h4>
+                                {itemDataFPD[1].title}
+                            </h4>
+                            <img
+                                srcSet={`${itemDataFPD[1].img}`}  //?w=250&h=250&fit=crop&auto=format&dpr=2 2x
+                                src={`${itemDataFPD[1].img}`}  // ?w=250&h=250&fit=crop&auto=format
+                                alt={itemDataFPD[1].title}
+                                loading="lazy"
+                            />
+                        </TabPanel>
+                        <TabPanel value="2">
+                            Таблица описания графа
+                            <TableContainer component={Paper}>
+                                <Table>
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell>#</TableCell>
+                                            <TableCell align="right"> ID</TableCell>
+                                            <TableCell align="right">Имя</TableCell>
+                                            <TableCell align="right">Тип</TableCell>
+                                            <TableCell align="right">Размеры</TableCell>
+                                            <TableCell align="right">Верхний и.</TableCell>
+                                            <TableCell align="right">Нижний и.</TableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        {listStorage.map((row) => (
+                                            <TableRow key={row.name}>
+                                                <TableCell component="th" scope="row">{row.id}</TableCell>
+                                                <TableCell align="right">{row.id}</TableCell>
+                                                <TableCell align="right">{row.name}</TableCell>
+                                                <TableCell align="right">{row.type}</TableCell>
+                                                <TableCell align="right">{row.x} : {row.y}</TableCell>
+                                                <TableCell align="right">{row.upperIndex}</TableCell>
+                                                <TableCell align="right">{row.lowerIndex}</TableCell>
+                                            </TableRow>
+                                        ))}
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
+                        </TabPanel>
+                    </TabContext>
                 </Grid>
-
-                <Grid item xs={12}>
-                    <ImageList sx={{
-                        width: "100%",
-                        height: 600,
-                        // Promote the list into its own layer in Chrome. 
-                        // This costs memory, but helps keeping high FPS.
-                        transform: 'translateZ(0)',
-                    }}
-                        rowHeight={200}
-                        gap={10}>
-
-                        {itemDataFPD.map((item) => (
-                            <ImageListItem key={item.img} cols={1} rows={itemDataFPD.length}>
-                                <img
-                                    srcSet={`${item.img}?w=500&fit=crop&auto=format&dpr=2 2x`}
-                                    src={`${item.img}?w=500&fit=crop&auto=format`}
-                                    alt={item.title}
-                                    loading="lazy"
-                                />
-                                <ImageListItemBar
-                                    title={item.title}
-                                    position="top"
-                                />
-                            </ImageListItem>
-                        ))}
-                    </ImageList>
+                <Grid item xs={6}>
+                <TextField
+                                id="filled-multiline-static"
+                                label="Описание устройства"
+                                multiline
+                                rows={20}
+                                fullWidth
+                                defaultValue={"xxx"}
+                                variant="filled"
+                            />
                 </Grid>
-
             </Grid>
+
         </Box>
 
 
